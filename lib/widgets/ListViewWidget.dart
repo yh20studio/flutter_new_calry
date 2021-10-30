c2ae97ae4ddf2aef7192042ccacab664119d50f3
2young <yh20studio@gmail.com>
2021-10-30 21:07:28 +0900

    Update New-calry Front-end

--- lib/widgets/ListViewWidget.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_webservice/class.dart';
-import 'package:flutter_webservice/detail/RoutinesMemosDetail.dart';
-import 'package:flutter_webservice/widgets/DateWidget.dart';
